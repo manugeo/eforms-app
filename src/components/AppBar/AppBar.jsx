dcd211d3c387ded56ecdd32f5a2a1bbf58063279
Manu Thomas <manu.thomas@eformsolutions.com>
2021-03-18 11:21:35 +0530

    Made appbar scrollable.

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, ScrollView } from 'react-native';
 import Constants from 'expo-constants';
 import AppBarTab from './AppBarTab';
 
@@ -7,11 +7,13 @@ import theme from '../../theme';
 
 const styles = StyleSheet.create({
   container: {
-    display: 'flex',
-    flexDirection: 'row',
     marginTop: Constants.statusBarHeight,
     padding: theme.padding.normal,
     backgroundColor: theme.colors.appBarBackgroundColor
+  },
+  scrollView: {
+    display: 'flex',
+    flexDirection: 'row'
   },
   marginLeft: {
     marginLeft: theme.margin.small
@@ -21,13 +23,15 @@ const styles = StyleSheet.create({
 const AppBar = () => {
   return (
     <View style={styles.container}>
-      <AppBarTab link="/" onPress={() => alert('Pressed Repositories')}>
-        Repositories
-      </AppBarTab>
+      <ScrollView style={styles.scrollView} horizontal>
+        <AppBarTab link="/" onPress={() => alert('Pressed Repositories')}>
+          Repositories
+        </AppBarTab>
 
-      <AppBarTab link="/sign-in" style={styles.marginLeft}>
-        Sign In
-      </AppBarTab>
+        <AppBarTab link="/sign-in" style={styles.marginLeft}>
+          Sign In
+        </AppBarTab>
+      </ScrollView>
     </View>
   );
 };
