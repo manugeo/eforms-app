8d0b00790cacfd03f60d40bc26248cdfd52d2d5b
Manu Thomas <manu.thomas@eformsolutions.com>
2021-04-01 17:05:55 +0530

    Implemented useAuthStorage hook.

--- src/hooks/useSignIn.js
+++ src/hooks/useSignIn.js
@@ -1,5 +1,11 @@
+import useAuthStorage from "./useAuthStorage";
+
 const useSignIn = () => {
+  const authStorage = useAuthStorage();
+  // Todo: After a successful sign-in, store access token to authStorage.
   const signIn = async ({username, password}) => {
+    // Todo: Handle fetch method correctly.
+    // See: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch#checking_that_the_fetch_was_successful
     const response = await fetch('http://66.45.252.83:8080/efscore/authenticate', {
       method: 'POST',
       headers: {
