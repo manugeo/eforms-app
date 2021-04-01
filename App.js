5102f0789209162548dfd27696487923a193c4e3
Manu Thomas <manu.thomas@eformsolutions.com>
2021-04-01 13:34:34 +0530

    Created AuthStorageContext and providing it in the App component.

--- App.js
+++ App.js
@@ -2,11 +2,17 @@ import React from 'react';
 import { NativeRouter } from 'react-router-native';
 
 import Main from './src/components/Main';
+import AuthStorageContext from './src/contexts/AuthStorageContext';
+import AuthStorage from './src/utils/authStorage';
+
+const authStorage = new AuthStorage();
 
 const App = () => {
   return (
     <NativeRouter>
-      <Main />
+      <AuthStorageContext.Provider value={authStorage}>
+        <Main />
+      </AuthStorageContext.Provider>
     </NativeRouter>
   );
 };
