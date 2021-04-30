f3d902e13681a88dc0f9318927b1374e1887e1d7
Manu Thomas <manu.thomas@eformsolutions.com>
2021-04-30 13:47:49 +0530

    Added react-native-paper.

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { NativeRouter } from 'react-router-native';
+import { Provider as PaperProvider } from 'react-native-paper';
 
 import Main from './src/components/Main';
 import AuthStorageContext from './src/contexts/AuthStorageContext';
@@ -11,7 +12,9 @@ const App = () => {
   return (
     <NativeRouter>
       <AuthStorageContext.Provider value={authStorage}>
-        <Main />
+        <PaperProvider>
+          <Main />
+        </PaperProvider>
       </AuthStorageContext.Provider>
     </NativeRouter>
   );
