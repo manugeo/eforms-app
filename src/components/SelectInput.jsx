905c7e1782d1c1c9928683f9871e2c6c0325ced5
Manu Thomas <manu.thomas@eformsolutions.com>
2021-05-11 19:24:55 +0530

    Updates to custom select input.

--- src/components/SelectInput.jsx
+++ src/components/SelectInput.jsx
@@ -1,28 +1,27 @@
-import React from 'react';
-import { TouchableWithoutFeedback, View } from 'react-native';
+import React, { useState } from 'react';
+import { TouchableWithoutFeedback, View, StyleSheet } from 'react-native';
 import { TextInput, Menu } from 'react-native-paper';
+import theme from '../theme';
 
-const SelectAnchor = ({ error, label, value, onPress, style, ...props }) => {
+
+const DEFAULT_MENU_ITEM_WIDTH = 240;
+
+const styles = StyleSheet.create({
+  anchorElement: {
+    height: 1
+  }
+});
+
+const AnchorElement = ({ onLayout }) => {
   return (
-    <TouchableWithoutFeedback onPress={onPress}>
-      <View style={style}>
-        <TextInput
-          mode='outlined'
-          value={value}
-          label={label}
-          error={error}
-          right={<TextInput.Icon name='menu-down' />}
-          editable={false}
-          {...props}
-        />
-      </View>
-    </TouchableWithoutFeedback>
+    <View onLayout={onLayout} style={styles.anchorElement}></View>
   );
 };
 
 const SelectInput = ({ value, error, label, style, options = [], onChangeValue = () => { }, onDismiss = () => { }, ...props }) => {
-  const [visible, setVisible] = React.useState(false);
-  const [selectValue, setSelectValue] = React.useState(value || null);
+  const [visible, setVisible] = useState(false);
+  const [selectValue, setSelectValue] = useState(value || null);
+  const [anchorWidth, setAnchorWidth] = useState(DEFAULT_MENU_ITEM_WIDTH);
   const openMenu = () => setVisible(true);
   const closeMenu = () => setVisible(false);
   const onItemPress = (value) => {
@@ -30,11 +29,19 @@ const SelectInput = ({ value, error, label, style, options = [], onChangeValue =
     setVisible(false);
     onChangeValue(value);
   };
-
+  const onAnchorLayoutChange = (event) => {
+    const width = event?.nativeEvent?.layout.width || DEFAULT_MENU_ITEM_WIDTH;
+    setAnchorWidth(width);
+  };
   const getMenuItems = (menuOptions) => {
     let menuItems = [];
     for (let option of menuOptions) {
-      menuItems.push(<Menu.Item onPress={() => onItemPress(option.value)} title={option.name} key={option.value} />);
+      menuItems.push(<Menu.Item
+        title={option.name}
+        key={option.value}
+        style={{ width: anchorWidth, maxWidth: anchorWidth }}
+        onPress={() => onItemPress(option.value)}
+      />);
     }
     return menuItems;
   };
@@ -45,23 +52,32 @@ const SelectInput = ({ value, error, label, style, options = [], onChangeValue =
   };
 
   return (
-    <Menu
-      visible={visible}
-      onDismiss={() => {
-        closeMenu();
-        onDismiss();
-      }}
-      anchor={<SelectAnchor
-        onPress={openMenu}
-        value={getOptionNameFromValue(selectValue)}
-        label={label}
-        error={error}
-        style={style}
-        {...props}
-      />}
-    >
-      {getMenuItems(options)}
-    </Menu>
+    <TouchableWithoutFeedback onPress={openMenu}>
+      <View style={style}>
+        <TextInput
+          mode='outlined'
+          selectionColor={theme.colors.textPrimary}
+          value={getOptionNameFromValue(selectValue)}
+          label={label}
+          error={error}
+          right={<TextInput.Icon name='menu-down' />}
+          editable={false}
+          {...props}
+        />
+
+        <Menu
+          visible={visible}
+          onDismiss={() => {
+            closeMenu();
+            onDismiss();
+          }}
+          anchor={<AnchorElement onLayout={onAnchorLayoutChange} />}
+          contentStyle={styles.menu}
+        >
+          {getMenuItems(options)}
+        </Menu>
+      </View>
+    </TouchableWithoutFeedback>
   );
 };
 
